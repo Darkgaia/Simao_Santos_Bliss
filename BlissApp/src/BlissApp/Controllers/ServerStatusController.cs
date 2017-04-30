72525cb1ca966244597b0c5e0c0107e656f120a7
Simon <simao_29@hotmail.com>
2017-04-30 20:58:29 +0100

    Server Health
    -Basic Server Health Setup
    -Added Verifications on PUT

---
+++ BlissApp/src/BlissApp/Controllers/ServerStatusController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BlissApp.Model;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BlissApp.Controllers
+{
+    
+
+    [Route("api/[controller]")]
+    public class ServerStatusController : Controller
+    {
+        private static ServerStatus stat = new ServerStatus
+        {
+            Status = "OK"
+        };
+
+        // GET: api/<controller>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            
+            return Json(stat);
+        }
+
+        // PUT api/<controller>
+        [HttpPut]
+        public void Put([FromBody]string value)
+        {
+            if (value == "OK" || value == "NOT OK")
+            {
+                stat.Status = value;
+                Response.StatusCode = 204;
+            }
+            else
+            {
+                Response.StatusCode = 400;
+                /*Add example of response*/
+            }
+        }
+    }
+}
---
+++ BlissApp/src/BlissApp/Model/ServerStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlissApp.Model
+{
+    public class ServerStatus
+    {
+        public string Status { get; set; }
+    }
+}
